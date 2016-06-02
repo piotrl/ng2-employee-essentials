85bfc8531f0a76e740b07551de9ab7a91d4d6deb
Piotr Lewandowski <grovman@gmail.com>
2016-06-02 22:38:50 +0200

    Dashboard component + Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import {Component}       from '@angular/core';
-import {EmployeeService}     from './employee/employee.service';
-import {EmployeesComponent} from './employee/employee.component';
+import {Component, OnInit} from "@angular/core";
+import {Router, Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "@angular/router";
+
+import {EmployeeService} from "./employee/employee.service";
+import {EmployeesComponent} from "./employee/employee.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+
+@Routes([
+    {path: '/', component: DashboardComponent},
+    {path: '/employees', component: EmployeesComponent}
+])
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <employees-list></employees-list>
-  `,
-    directives: [EmployeesComponent],
-    providers: [EmployeeService]
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS, EmployeeService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Employee Essentials';
+
+    constructor(private router: Router) {}
+
+    ngOnInit() {
+        this.router.navigate(['/']);
+    }
 }
